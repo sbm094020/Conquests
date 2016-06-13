c2192be5628a095f5395f3efbaa45268d5af547a
sbm094020 <sbm094020@utdallas.edu>
2016-06-12 19:13:36 -0500

    Saving state before first remote commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/**eslint-env node */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+  	console.log('Gulp task executed');
+});
+
+gulp.task('styles', function(){
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));		
+});
